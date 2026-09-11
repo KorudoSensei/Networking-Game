772eae0c62c213d1083da89a9189b42d80afbc54
KorudoSensei <yiyang2909@gmail.com>
2026-09-12 03:18:29 +0800

    Hosting and Client UI

--- Assets/Scripts/NetworkUiManager.cs
+++ Assets/Scripts/NetworkUiManager.cs
@@ -1,10 +1,25 @@
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkUiManager : MonoBehaviour
 {
     [SerializeField] private Button hostButton, clientButton;
+    // private string targetIP, serverIP;
+    // private ushort targetPort, serverPort;
+
+    [Header("Client/Target Settings")]
+    [SerializeField] private InputField targetIPInputField;
+    [SerializeField] private InputField targetPortInputField;
+
+    [Header("Server/Host Settings")]
+    [SerializeField] private InputField serverIPInputField;
+    [SerializeField] private InputField serverPortInputField;
+
+    [Header("Defaults")]
+    [SerializeField] private string defaultIP = "127.0.0.1";
+    [SerializeField] private ushort defaultPort = 7777;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +29,34 @@ public class NetworkUiManager : MonoBehaviour
 
     private void HostButtonOnClick()
     {
+        string ip = string.IsNullOrWhiteSpace(serverIPInputField.text) ? "0.0.0.0" : serverIPInputField.text.Trim();
+        ushort port = GetPortFromInput(serverIPInputField);
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ip, port, "0.0.0.0");
         NetworkManager.Singleton.StartHost();
+
+       Debug.Log($"Hosting on Server IP: {ip}, Port: {port}");
     }
 
     private void ClientButtonOnClick()
     {
+        string ip = string.IsNullOrWhiteSpace(targetIPInputField.text) ? defaultIP : targetIPInputField.text.Trim();
+        ushort port = GetPortFromInput(targetPortInputField);
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ip, port);
         NetworkManager.Singleton.StartClient();
+        
+        Debug.Log($"Connecting to Target IP: {ip}, Port: {port}");
+    }
+
+    private ushort GetPortFromInput(InputField portField)
+    {
+        if(portField != null && !string.IsNullOrWhiteSpace(portField.text))
+        {
+            ushort.TryParse(portField.text.Trim(), out ushort parsedPort);
+            return parsedPort;
+        }
+
+        return defaultPort;
     }
 }
