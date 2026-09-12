3ebde89051f41ca9707ddaa943762fa33231e32b
KorudoSensei <yiyang2909@gmail.com>
2026-09-12 20:23:18 +0800

    bugfixes and playerdata

--- Assets/Scripts/NetworkUiManager.cs
+++ Assets/Scripts/NetworkUiManager.cs
@@ -12,14 +12,19 @@ public class NetworkUiManager : MonoBehaviour
     [Header("Client/Target Settings")]
     [SerializeField] private InputField targetIPInputField;
     [SerializeField] private InputField targetPortInputField;
+    [SerializeField] private InputField clientUsernameInputField;
 
     [Header("Server/Host Settings")]
     [SerializeField] private InputField serverIPInputField;
     [SerializeField] private InputField serverPortInputField;
+    [SerializeField] private InputField hostUsernameInputField;
 
     [Header("Defaults")]
     [SerializeField] private string defaultIP = "127.0.0.1";
     [SerializeField] private ushort defaultPort = 7777;
+
+
+    public static string EnteredUsername;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,12 +35,15 @@ public class NetworkUiManager : MonoBehaviour
     private void HostButtonOnClick()
     {
         string ip = string.IsNullOrWhiteSpace(serverIPInputField.text) ? "0.0.0.0" : serverIPInputField.text.Trim();
-        ushort port = GetPortFromInput(serverIPInputField);
+        ushort port = GetPortFromInput(serverPortInputField);
 
+        EnteredUsername = SaveUsername(hostUsernameInputField);
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ip, port, "0.0.0.0");
         NetworkManager.Singleton.StartHost();
 
-       Debug.Log($"Hosting on Server IP: {ip}, Port: {port}");
+
+
+        Debug.Log($"Hosting on Server IP: {ip}, Port: {port}");
     }
 
     private void ClientButtonOnClick()
@@ -45,6 +53,8 @@ public class NetworkUiManager : MonoBehaviour
 
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ip, port);
         NetworkManager.Singleton.StartClient();
+
+        EnteredUsername = SaveUsername(clientUsernameInputField);
         
         Debug.Log($"Connecting to Target IP: {ip}, Port: {port}");
     }
@@ -58,5 +68,17 @@ public class NetworkUiManager : MonoBehaviour
         }
 
         return defaultPort;
+    }
+
+    private string SaveUsername(InputField nameField)
+    {
+        if (nameField != null && !string.IsNullOrWhiteSpace(nameField.text))
+        {
+            return EnteredUsername = nameField.text.Trim();
+        }
+        else
+        {
+            return EnteredUsername = $"Player {Random.Range(10, 99)}";
+        }
     }
 }
--- Assets/Scripts/PlayerNetworkData.cs
+++ Assets/Scripts/PlayerNetworkData.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
-public class PlayerNetworkData : MonoBehaviour
+public class PlayerNetworkData : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public NetworkVariable<FixedString64Bytes> username = new NetworkVariable<FixedString64Bytes>
+    ("Unassigned", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    [SerializeField] private Text nameDisplayText;
+    
+
+    public override void OnNetworkSpawn()
     {
-        
+        username.OnValueChanged += OnNameChanged;
+        UpdateNameDisplay(username.Value.ToString());
+
+        if(IsOwner)
+        {
+            string typedName = NetworkUiManager.EnteredUsername;
+            SetUsernameServerRpc(typedName);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkDespawn()
     {
-        
+        username.OnValueChanged -= OnNameChanged;
+    }
+    private void UpdateNameDisplay(string name)
+    {
+        if (nameDisplayText != null)
+        {
+            nameDisplayText.text = name;
+        }
+    }
+
+    private void OnNameChanged(FixedString64Bytes oldval, FixedString64Bytes newval)
+    {
+        UpdateNameDisplay(newval.ToString());
+    }
+
+    [ServerRpc]
+    private void SetUsernameServerRpc(string name)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            username.Value = name.Trim();
+        }
     }
 }
